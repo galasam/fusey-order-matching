0110eff69e77e14f1f1430bcc6ce238f8957390f
srussell <samuel.russell@galatea-associates.com>
2019-03-15 08:54:45 +0000

    use sl4j logger

--- src/main/java/com/gala/sam/lambSauce/service/MarketService.java
+++ src/main/java/com/gala/sam/lambSauce/service/MarketService.java
@@ -18,11 +18,10 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.SortedSet;
 import java.util.TreeMap;
-import java.util.logging.Logger;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MarketService {
-
-  final private static Logger LOGGER = Logger.getLogger("MARKET_LOGGER");
 
   private List<Trade> trades = new ArrayList<>();
   private Map<String, TickerData> tickerQueues = new TreeMap<>();
@@ -35,13 +34,13 @@ public class MarketService {
   }
 
   public void completeTimestep(Order order) {
-    LOGGER.finer("Processing Triggered Stop Orders");
+    log.info("Processing Triggered Stop Orders");
     processOrder(order);
     processTriggeredStopOrders();
   }
 
   private void processOrder(Order order) {
-    LOGGER.finer(String.format("Processing order %s", order.toString()));
+    log.info(String.format("Processing order %s", order.toString()));
     if(order instanceof StopOrder) {
       stopOrders.add((StopOrder) order);
     } else if(order instanceof LimitOrder) {
@@ -51,23 +50,23 @@ public class MarketService {
     } else {
       throw new UnsupportedOperationException("Order type not specified");
     }
-    LOGGER.finer("Ticker queues: " + tickerQueues.toString());
-    LOGGER.finer("Stop Orders: " + stopOrders.toString());
-    LOGGER.finer("Trades: " + trades.toString());
+    log.info("Ticker queues: " + tickerQueues.toString());
+    log.info("Stop Orders: " + stopOrders.toString());
+    log.info("Trades: " + trades.toString());
   }
 
   private void processTriggeredStopOrders() {
     Iterator<StopOrder> it = stopOrders.iterator();
     while(it.hasNext()) {
       StopOrder stopOrder = it.next();
-      LOGGER.finer("Testing Trigger on: " + stopOrder.toString());
+      log.info("Testing Trigger on: " + stopOrder.toString());
       if(isStopLossTriggered(stopOrder)) {
-        LOGGER.finer("Stop Order Triggered");
+        log.info("Stop Order Triggered");
         it.remove();
         ReadyOrder readyOrder = stopOrder.getReadyOrder();
         processOrder(readyOrder);
       } else {
-        LOGGER.finer("Stop Order not Triggered");
+        log.info("Stop Order not Triggered");
       }
     }
   }
@@ -75,20 +74,20 @@ public class MarketService {
   private boolean isStopLossTriggered(StopOrder stopOrder) {
     ReadyOrder readyOrder = stopOrder.getReadyOrder();
     Optional<Float> lastExec = getTickerQueueGroup(readyOrder).getLastExecutedTradePrice();
-    LOGGER.finest("Checking if there has been a previous trade");
+    log.debug("Checking if there has been a previous trade");
     if(lastExec.isPresent()) {
-      LOGGER.finest("Previous trade found, checking direction");
+      log.debug("Previous trade found, checking direction");
       if(readyOrder.getDirection().equals(DIRECTION.BUY)) {
-        LOGGER.finest("Buy direction: testing trigger");
+        log.debug("Buy direction: testing trigger");
         return stopOrder.getTriggerPrice() <= lastExec.get();
       } else if(readyOrder.getDirection().equals(DIRECTION.SELL)) {
-        LOGGER.finest("Sell direction: testing trigger");
+        log.debug("Sell direction: testing trigger");
         return stopOrder.getTriggerPrice() >= lastExec.get();
       } else {
         throw new UnsupportedOperationException("Order direction not supported");
       }
     } else {
-      LOGGER.finest("No previous trade found");
+      log.debug("No previous trade found");
       return false;
     }
   }
@@ -117,13 +116,13 @@ public class MarketService {
 
   private void processDirectedMarketOrder(MarketOrder marketOrder, TickerData tickerData,
       SortedSet<LimitOrder> limitOrders, SortedSet<MarketOrder> marketOrders) {
-    LOGGER.finest("Checking Limit Order queue");
+    log.debug("Checking Limit Order queue");
     if(limitOrders.isEmpty()) {
-      LOGGER.finest("Limit Order queue empty, so check if time in force");
+      log.debug("Limit Order queue empty, so check if time in force");
       queueIfTimeInForce(marketOrder, marketOrders);
     } else {
       LimitOrder limitOrder = limitOrders.first();
-      LOGGER.finest("Limit Order queue not empty, so trading with best limit order: " + limitOrder.toString());
+      log.debug("Limit Order queue not empty, so trading with best limit order: " + limitOrder.toString());
       limitOrders.remove(limitOrder);
       makeTrade(marketOrder, limitOrder, limitOrder.getLimit(), tickerData);
     }
@@ -138,7 +137,7 @@ public class MarketService {
           .matchQuantity(a.getQuantity())
           .matchPrice(limit)
           .build();
-      LOGGER.finest("Making Buy trade: " + trade.toString());
+      log.debug("Making Buy trade: " + trade.toString());
       trades.add(trade);
     } else if(a.getDirection().equals(DIRECTION.SELL)) {
       Trade trade = Trade.builder()
@@ -147,7 +146,7 @@ public class MarketService {
           .matchQuantity(a.getQuantity())
           .matchPrice(limit)
           .build();
-      LOGGER.finest("Making Sell trade: " + trade.toString());
+      log.debug("Making Sell trade: " + trade.toString());
       trades.add(trade);
     } else {
       throw new UnsupportedOperationException("Order direction not supported");
@@ -175,27 +174,27 @@ public class MarketService {
       SortedSet<MarketOrder> marketOrders,
       SortedSet<LimitOrder> sameTypeLimitOrders,
       SortedSet<LimitOrder> oppositeTypeLimitOrders) {
-    LOGGER.finest("Checking main.Market Order queue");
+    log.debug("Checking main.Market Order queue");
     if(marketOrders.isEmpty()) {
-      LOGGER.finest("main.Market Order queue empty, so checking Limit orders");
+      log.debug("main.Market Order queue empty, so checking Limit orders");
       if(oppositeTypeLimitOrders.isEmpty()) {
-        LOGGER.finest("Limit Order queue empty, so check if time in force");
+        log.debug("Limit Order queue empty, so check if time in force");
         queueIfTimeInForce(limitOrder, sameTypeLimitOrders);
       } else {
         LimitOrder otherLimitOrder = oppositeTypeLimitOrders.first();
-        LOGGER.finest("Limit Order queue not empty, so checking if best order matches: " + otherLimitOrder.toString());
+        log.debug("Limit Order queue not empty, so checking if best order matches: " + otherLimitOrder.toString());
 
         if(limitOrder.limitMatches(otherLimitOrder)) {
-          LOGGER.finest("Limits match so completing trade");
+          log.debug("Limits match so completing trade");
           oppositeTypeLimitOrders.remove(otherLimitOrder);
           makeTrade(limitOrder, otherLimitOrder, otherLimitOrder.getLimit(), tickerData);
         } else {
-          LOGGER.finest("Limits do not match, so check if time in force");
+          log.debug("Limits do not match, so check if time in force");
           queueIfTimeInForce(limitOrder, sameTypeLimitOrders);
         }
       }
     } else {
-      LOGGER.finest("main.Market Order queue not empty, so trading with oldest order: " + limitOrder.toString());
+      log.debug("main.Market Order queue not empty, so trading with oldest order: " + limitOrder.toString());
       MarketOrder marketOrder = marketOrders.first();
       marketOrders.remove(marketOrder);
       makeTrade(marketOrder, limitOrder, limitOrder.getLimit(), tickerData);
